54a20b5f7453a7f55124204d6b42d469843c70a2
Daniel Vallejo <daniel_vallejoquiin@hotmail.com>
2017-09-11 11:03:42 -0400

    checar

---
+++ Pregunta.java
@@ -0,0 +1,29 @@
+
+/**
+ * proyecto para varias preguntas 
+ * 
+ * 
+ */
+
+public class Pregunta
+{
+    private String texto;
+    private String respuesta;
+    
+    public void IngresaPregunta(String pregunta)
+    {
+        texto = pregunta;
+    }
+    
+    public void asignaRespuesta(String unresp)
+    {
+        respuesta = respuesta;
+    }
+    
+    public Pregunta()
+    {
+        texto = " ";
+        respuesta = " ";
+    }
+}
+
